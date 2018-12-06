ae529a72c87b4033c2f06851fa1c698482cb81f7
Assad Akram <assadakram.iintellect@gmail.com>
2018-12-06 21:00:00 +0500

    First commit

--- components/App/index.js
+++ components/App/index.js
@@ -5,7 +5,7 @@ class App extends Component{
     render(){
         return(
             <div>
-                <p>App component</p>
+                <p>App component from main</p>
             </div>
         )
     }

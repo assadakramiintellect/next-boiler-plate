c3b482939e7be0eae01a0990aa31d676330c0f15
Assad Akram <assadakram.iintellect@gmail.com>
2018-12-06 20:52:04 +0500

    First commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import '../style.css';
+import App from "../components/App";
+class index extends Component{
+    render(){
+        return(
+            <App/>
+        )
+    }
+}
+
+export default index;

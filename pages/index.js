cef9eb7ab46db67427218a89052ba0efef80704a
aqkhan <aqk.neo@gmail.com>
2019-02-08 15:48:14 +0500

    packages updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import '../style.css';
-import App from "../components/App";
+import App from "../components/app";
 class index extends Component{
     render(){
         return(
